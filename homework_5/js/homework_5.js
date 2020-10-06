95360865eebc1c7c28aea31cf36987ea8e2bc389
Dmitriy Safronov <svsdima1995@mail.ru>
2020-10-06 11:26:41 +0300

    Пятая домашняя работа

---
+++ homework_5/js/homework_5.js
@@ -0,0 +1,63 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+"use strict";
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+
+// --------------------------------------------------------------------
+// 1)
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach (item => {
+    item.remove();
+});
+
+
+// 2)
+genre.textContent = 'драма';
+
+// 3)
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+// 4)
+/* Удаляем старый текст */
+movieList.innerHTML = "";
+
+/* Сортируем по алфавиту */
+movieDB.movies.sort();
+
+/* += дополнительное присваивание (a = a + 1; a += 1;) */
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
+
+
+
